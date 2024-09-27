6ceec0190cdefd1461bad3151b6c062b0807fb29
Gabriel Flores <gabgabflores2@gmail.com>
2024-09-27 20:55:44 +0800

    final code

--- DSA_exercise2.py
+++ DSA_exercise2.py
@@ -9,19 +9,19 @@ if choice == 1 :
     resistance = int(input("Resistance: "))
     current = int(input("Current: "))
     answer = resistance*current
-    print(f"Huwaw the Voltage is {answer}")
+    print(f"The Voltage is {answer}")
 
 elif choice == 2 :
     voltage = int(input("Voltage: "))
     current = int(input("Current: "))
     answer = voltage/current
-    print(f"Huwaw the Resistance is {answer}")
+    print(f"The Resistance is {answer}")
     
 elif choice == 3 :
     voltage = int(input("Voltage: "))
     resistance = int(input("Resistance: "))
     answer = voltage/resistance
-    print(f"Huwaw the Resistance is {answer}")
+    print(f"The Resistance is {answer}")
     
 else:
-    print("U dumb?")
+    print("Please enter a valid number")
